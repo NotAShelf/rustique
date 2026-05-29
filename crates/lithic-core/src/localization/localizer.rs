2b4f82c35a5b043f6e796469b9934192f858a087
NotAShelf <raf@notashelf.dev>
2026-05-29 16:21:22 +0300

    lithic-core: remove TOML localization stub; fix duplicate `comfy_table` import in utils
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: Ia1b67be1b889504ec8f838ee0050d4c96a6a6964

--- crates/lithic-core/src/localization/localizer.rs
+++
@@ -1,9 +0,0 @@
-use clap::Arg;
-
-pub fn handle_about_text(cmd: clap::Command) -> clap::Command {
-    cmd.mut_arg("verbose", test_verbose)
-}
-
-fn test_verbose(arg: Arg) -> Arg {
-    arg.help("Shows info level logging messages. This is very noisy, used for debugging purposes.")
-}
